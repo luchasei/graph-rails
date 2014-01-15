172aeec1ef2b2a3d37d065bbd6250a195a375620
Eddy <Eddy@thehackerati.com>
2014-01-15 15:37:57 -0500

    new comment

--- app/worker/twitter_worker.rb
+++ app/worker/twitter_worker.rb
@@ -3,6 +3,7 @@ class TwitterWorker
   sidekiq_options retry: false
   
   #Async worker to get a tweet
+  #Fires according to the time schedule in schedule.rb 
   def perform()
     Tweet.get_tweets
   end
